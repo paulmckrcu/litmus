135162a0dd3738714a476f6e1dcdada44eab773f
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2015-12-27 14:08:53 -0800

    Enable parallel runs of tests
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- RCUlitmusGen/RunAllLitmus.sh
+++ RCUlitmusGen/RunAllLitmus.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Run the automatically generated litmus tests in the litmus/ directory.
 #
 # Usage:
-#	sh RunAllLitmus.sh < makelitmustests.sh.out
+#	sh RunAllLitmus.sh [ ncpus ] < makelitmustests.sh.out
 #
 # Can specify LINUX_BELL_FILE and LINUX_CAT_FILE environment variables.
 #
@@ -25,6 +25,47 @@
 #
 # Author: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
 
+nonline=`getconf _NPROCESSORS_ONLN`
+ncpu=${1-$nonline}
+
+T=/tmp/RunAllLitmus.sh.$$
+# trap 'rm -rf $T' 0
+mkdir $T
+
+for ((i=0;i<$ncpu;i++))
+do
+	touch $T/asExpected.$i
+	touch $T/notAsExpected.$i
+	echo T=$T > $T/$i.sh
+	cat << '___EOF___' >> $T/$i.sh
+	runtest () {
+		echo ' ... ' sh RunLitmus.sh $1.litmus $2
+		if sh RunLitmus.sh $1.litmus $2
+		then
+___EOF___
+			echo 'echo "$asExpected $1:($2)" >> ' $T/asExpected.$i >> $T/$i.sh
+		echo else >> $T/$i.sh
+			echo 'echo "$notAsExpected $1:($2)" >> ' $T/notAsExpected.$i >> $T/$i.sh
+		echo fi >> $T/$i.sh
+	echo } >> $T/$i.sh
+done
+
 sed -e 's/\.litmus//' |
-	awk '{ print "runtest " $2 " " $4; }' > shortlitmus.sh
-sh runAllCat.sh
+awk -v ncpu=$ncpu -v t=$T '
+{
+	print "runtest " $2 " " $4 >> t "/" NR % ncpu ".sh";
+}
+
+END {
+	for (i = 0; i < ncpu; i++) {
+		print "sh " t "/" i ".sh > " t "/" i ".sh.out 2>&1 &";
+		close(t "/" i ".sh")
+	}
+	print "wait";
+
+}' | sh
+asExpected=`cat $T/asExpected.*`
+notAsExpected=`cat $T/notAsExpected.*`
+cat $T/*.sh.out
+. ./RunReduce.sh
+ls $T
