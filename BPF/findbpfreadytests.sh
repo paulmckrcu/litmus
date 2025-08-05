0365ef3b7a0afad1fbc2f6aa0047a119a546eac3
Paul E. McKenney <paulmck@kernel.org>
2025-08-05 16:24:45 -0700

    findbpfreadytests.sh: Allow simple "if" statements
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- BPF/findbpfreadytests.sh
+++ BPF/findbpfreadytests.sh
@@ -19,7 +19,7 @@ find ${litmus_tree} -name '*.litmus' -print | grep -ve '-BPF\.litmus$' | mselect
 xargs < $T/list-C -r grep -L 'rcu_read_lock();\|rcu_read_unlock();\|rcu_dereference(.*);\|rcu_assign_pointer(.*);\|synchronize_rcu();\|synchronize_rcu_expedited();\|srcu_read_lock([^)]*);\|srcu_read_unlock([^)]*);\|srcu_down_read([^)]*);\|srcu_up_read([^)]*);\|synchronize_srcu([^)]*);\|synchronize_srcu_expedited([^)]*);\|smp_rmb();\|smp_wmb();\|smp_mb__after_spinlock();\|smp_mb__after_unlock_lock();\|smp_mb__after_srcu_read_unlock();\|spin_lock([^)]*);\|spin_unlock([^)]*);\|spin_trylock([^)]*);spin_is_locked([^)]*);\|smp_memb();' > $T/list-C-bpf1
 
 # Discard tests using things to be implemented later on.
-xargs < $T/list-C-bpf1 -r grep -L '^[ 	]*if *(\|smp_store_mb(.*);' > $T/list-C-bpf2
+xargs < $T/list-C-bpf1 -r grep -L '^[ 	]*if *(.*{\|\^[	 ]*}* else\>\|smp_store_mb(.*);' > $T/list-C-bpf2
 
 # Take only tests with a "Result:" specified that is consistent with BPF
 # assembly language: No data races and no deadlocks.
